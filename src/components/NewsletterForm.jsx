0b31d904267c0a6cb8ac9e84e46d98dd71b13320
Rhoda Ojetola <momirhoda@gmail.com>
2025-06-15 17:00:54 +0100

    updating

--- src/components/NewsletterForm.jsx
+++ src/components/NewsletterForm.jsx
@@ -42,7 +42,7 @@ const NewsletterForm = () => {
       <button
         type="submit"
         disabled={disabled}
-        className="bg-green-700 text-white px-4 py-2 rounded-md hover:bg-green-800 transition"
+        className="bg-green-700 text-white px-4 py-2 rounded-md hover:bg-green-800 transition cursor-pointer"
       >
         Subscribe
       </button>
